2c5e81cf897412328efd3c104ddb8754683630f4
Adrian Strugala <strugala.adrian@gmail.com>
2019-03-25 11:44:26 +0100

    the program was able to decode simple avro file

--- AvroConvert/AvroConvert.Deserialize.cs
+++ AvroConvert/AvroConvert.Deserialize.cs
@@ -1,13 +1,18 @@
 ﻿namespace AvroConvert
 {
+    using Avro.IO;
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
     using System.IO;
     using System.Linq;
-    using Avro.IO;
+
 
     public static partial class AvroConvert
     {
+        private const int MarkerLength = 16;
+        private const int StopBytesLength = 1;
+
         public static T Deserialize<T>(byte[] avroString)
         {
             T result = default(T);
@@ -25,28 +30,42 @@
         {
             List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
 
+            byte[] avroHeader = avroString.Take(4).ToArray();
+            Contract.Assert(avroHeader[0] == 79);
+            Contract.Assert(avroHeader[1] == 98);
+            Contract.Assert(avroHeader[2] == 106);
+            Contract.Assert(avroHeader[3] == 1);
+
             int indexOfStopByte = Array.IndexOf(avroString, new byte());
             var schemaBytes = avroString.Take(indexOfStopByte).ToArray();
-            var dataBytes = avroString.Skip(indexOfStopByte+1).ToArray();
-
+            var fileMarkerBytes = avroString.Skip(indexOfStopByte + StopBytesLength).Take(MarkerLength);
+            var dataBytes = avroString.Skip(indexOfStopByte + StopBytesLength + MarkerLength).ToArray();
 
             var structure = AvroConvert.GetStructure(System.Text.Encoding.UTF8.GetString(schemaBytes));
 
             Stream stream = new MemoryStream(dataBytes);
 
             var binaryDecoder = new BinaryDecoder(stream);
-        //    var string1 = binaryDecoder.ReadString();
-         //   var string2 = binaryDecoder.ReadString();
-         //   var long1 = binaryDecoder.ReadLong();
 
-         var sth = ReadString(dataBytes);
+
+            var numberOfObjects = binaryDecoder.ReadLong();
+            var sizeOfObjects = binaryDecoder.ReadLong();
+
+            var string1 = binaryDecoder.ReadString();
+            var string2 = binaryDecoder.ReadString();
+            var long1 = binaryDecoder.ReadLong();
+
+            var string3 = binaryDecoder.ReadString();
+            var string4 = binaryDecoder.ReadString();
+            var long2 = binaryDecoder.ReadLong();
+
 
             return result;
         }
 
         public static string ReadString(byte[] data)
         {
-            int length = data[0];
+            int length = data[0] / 2;
             byte[] buffer = new byte[length];
             buffer = data.Take(length).ToArray();
             return System.Text.Encoding.UTF8.GetString(buffer);

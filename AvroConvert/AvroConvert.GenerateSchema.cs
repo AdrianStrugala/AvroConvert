d4c635f59004af09b8fb87d1eebca4bf48b299c3
Adrian Strugala <adrian.strugala@comparex.com>
2019-04-03 13:39:02 +0200

    this library may be the goal

--- AvroConvert/AvroConvert.GenerateSchema.cs
+++ AvroConvert/AvroConvert.GenerateSchema.cs
@@ -2,11 +2,17 @@
 {
     using Microsoft.Hadoop.Avro;
     using System;
+    using System.Linq;
+    using FastDeepCloner;
+    using Newtonsoft.Json;
 
     public static partial class AvroConvert
     {
         public static string GenerateSchema(object obj)
         {
+            var prop = new DeepCloner.GetFastDeepClonerProperties(typeof(test)).First();
+            prop.Attributes.Add(new JsonIgnoreAttribute());
+
             var createMethod = typeof(AvroSerializer).GetMethod("Create", new Type[0]);
             var createGenericMethod = createMethod.MakeGenericMethod(obj.GetType());
             dynamic avroSerializer = createGenericMethod.Invoke(obj, null);

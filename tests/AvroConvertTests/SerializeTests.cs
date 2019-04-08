7d4f7f5231bef90504a070cc88c8cc07d607896e
Adrian Strugala <adrian.strugala@comparex.com>
2019-04-08 13:27:23 +0200

    add test classes to tests

--- tests/AvroConvertTests/SerializeTests.cs
+++ tests/AvroConvertTests/SerializeTests.cs
@@ -1,9 +1,6 @@
 ﻿namespace AvroConvertTests
 {
     using System.Collections.Generic;
-    using System.Reflection.Emit;
-    using System.Runtime.Serialization;
-    using Microsoft.Hadoop.Avro;
     using Xunit;
 
     public class SerializeTests
@@ -12,32 +9,32 @@
         public void Serialize_InputIsList_NoExceptionIsThrown()
         {
             //Arrange
-            Dupa dupa = new Dupa
+            SomeTestClass someTestClass = new SomeTestClass
             {
-                cycek = new Cycki
+                objectProperty = new NestedTestClass
                 {
-                    lewy = "spoko",
-                    prawy = 2137
+                    justSomeProperty = "spoko",
+                    andLongProperty = 2137
                 },
-                numebr = 111111
+                simpleProperty = 111111
             };
 
-            Dupa dupa2 = new Dupa
+            SomeTestClass someTestClass2 = new SomeTestClass
             {
-                cycek = new Cycki
+                objectProperty = new NestedTestClass
                 {
-                    lewy = "loko",
-                    prawy = 2137
+                    justSomeProperty = "loko",
+                    andLongProperty = 2137
                 },
-                numebr = 2135
+                simpleProperty = 2135
             };
 
-            List<Dupa> dupas = new List<Dupa>();
-            dupas.Add(dupa);
-            dupas.Add(dupa2);
+            List<SomeTestClass> someTestClasses = new List<SomeTestClass>();
+            someTestClasses.Add(someTestClass);
+            someTestClasses.Add(someTestClass2);
 
             //Act
-            var result = AvroConvert.AvroConvert.Serialize(dupas);
+            var result = AvroConvert.AvroConvert.Serialize(someTestClasses);
 
             //Assert
             Assert.NotNull(result);
@@ -47,32 +44,32 @@
         public void Serialize_InputIsArray_NoExceptionIsThrown()
         {
             //Arrange
-            Dupa dupa = new Dupa
+            SomeTestClass someTestClass = new SomeTestClass
             {
-                cycek = new Cycki
+                objectProperty = new NestedTestClass
                 {
-                    lewy = "spoko",
-                    prawy = 2137
+                    justSomeProperty = "spoko",
+                    andLongProperty = 2137
                 },
-                numebr = 111111
+                simpleProperty = 111111
             };
 
-            Dupa dupa2 = new Dupa
+            SomeTestClass dupa2 = new SomeTestClass
             {
-                cycek = new Cycki
+                objectProperty = new NestedTestClass
                 {
-                    lewy = "loko",
-                    prawy = 2137
+                    justSomeProperty = "loko",
+                    andLongProperty = 2137
                 },
-                numebr = 2135
+                simpleProperty = 2135
             };
 
-            Dupa[] dupas = new Dupa[2];
-            dupas[0] = dupa;
-            dupas[1] = dupa2;
+            SomeTestClass[] someTestClasses = new SomeTestClass[2];
+            someTestClasses[0] = someTestClass;
+            someTestClasses[1] = dupa2;
 
             //Act
-            var result = AvroConvert.AvroConvert.Serialize(dupas);
+            var result = AvroConvert.AvroConvert.Serialize(someTestClasses);
 
             //Assert
             Assert.NotNull(result);
@@ -94,35 +91,11 @@
             //Assert
             Assert.NotNull(result);
         }
-    }
 
+        [Fact]
+        public void Serialize_SomeOneDefinedAvroAttributes_DefinedAreTaken()
+        {
 
-      [DataContract(Name = "Dupa", Namespace = "test.demo")]
-    public class User
-    {
-        public string name { get; set; }
-        [DataMember(Name = "favorite_number")]
-        [NullableSchema]
-        public int? favorite_number { get; set; }
-        public string favorite_color { get; set; }
-    }
-
-    [DataContract(Name = "Dupa", Namespace = "test.demo")]
-    public class Dupa
-    {
-        [DataMember(Name = "cycek")]
-        public Cycki cycek { get; set; }
-
-        [DataMember(Name = "numebr")]
-        public int numebr { get; set; }
-    }
-
-    [DataContract(Name = "Cycek", Namespace = "pubsub.demo")]
-    public class Cycki
-    {
-        [DataMember(Name = "lewy")]
-        public string lewy { get; set; }
-        [DataMember(Name = "prawy")]
-        public long prawy { get; set; }
+        }
     }
 }
---
+++ tests/AvroConvertTests/TestClasses.cs
@@ -0,0 +1,34 @@
+﻿namespace AvroConvertTests
+{
+    using System.Runtime.Serialization;
+    using Microsoft.Hadoop.Avro;
+
+
+    [DataContract(Name = "User", Namespace = "user")]
+    public class User
+    {
+        [DataMember(Name = "name")]
+        public string name { get; set; }
+
+        [DataMember(Name = "favorite_number")]
+        [NullableSchema]
+        public int? favorite_number { get; set; }
+
+        [DataMember(Name = "favorite_color")]
+        public string favorite_color { get; set; }
+    }
+
+    public class SomeTestClass
+    {
+        public NestedTestClass objectProperty { get; set; }
+
+        public int simpleProperty { get; set; }
+    }
+
+    public class NestedTestClass
+    {
+        public string justSomeProperty { get; set; }
+
+        public long andLongProperty { get; set; }
+    }
+}

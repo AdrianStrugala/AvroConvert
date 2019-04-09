0d769e39ac67a61cfffc5159575a8bfb1f0fcfe3
Adrian Strugala <adrian.strugala@comparex.com>
2019-04-09 14:20:25 +0200

    progress in deserialization

--- AvroConvert/AvroConvert.Deserialize.cs
+++ AvroConvert/AvroConvert.Deserialize.cs
@@ -8,9 +8,6 @@
 
     public static partial class AvroConvert
     {
-        private const int MarkerLength = 16;
-        private const int StopBytesLength = 1;
-
         public static List<Dictionary<string, object>> Deserialize(byte[] avroBytes)
         {
             List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
@@ -19,7 +16,6 @@
 
             var xd = reader.GetEntries().ToList();
 
-            // result.Add(xd[0]);
 
             return result;
         }
@@ -37,44 +33,5 @@
 
             return result;
         }
-
-
-
-        //        public static List<Dictionary<string, object>> Deserialize(byte[] avroString)
-        //        {
-        //            List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
-
-        //            byte[] avroHeader = avroString.Take(4).ToArray();
-        //            Contract.Assert(avroHeader[0] == 79);
-        //            Contract.Assert(avroHeader[1] == 98);
-        //            Contract.Assert(avroHeader[2] == 106);
-        //            Contract.Assert(avroHeader[3] == 1);
-        //
-        //            int indexOfStopByte = Array.IndexOf(avroString, new byte());
-        //            var schemaBytes = avroString.Take(indexOfStopByte).ToArray();
-        //            var fileMarkerBytes = avroString.Skip(indexOfStopByte + StopBytesLength).Take(MarkerLength);
-        //            var dataBytes = avroString.Skip(indexOfStopByte + StopBytesLength + MarkerLength).ToArray();
-        //
-        //            var structure = AvroConvert.GetSchema(System.Text.Encoding.UTF8.GetString(schemaBytes));
-        //
-        //            Stream stream = new MemoryStream(dataBytes);
-        //
-        //            var binaryDecoder = new BinaryDecoder(stream);
-        //
-        //
-        //            var numberOfObjects = binaryDecoder.ReadLong();
-        //            var sizeOfObjects = binaryDecoder.ReadLong();
-        //
-        //            var string1 = binaryDecoder.ReadString();
-        //            var int1 = binaryDecoder.ReadInt();
-        //            var string2 = binaryDecoder.ReadString();
-        //var long1 = binaryDecoder.ReadLong();
-        //
-        //            var string3 = binaryDecoder.ReadString();
-        //            var string4 = binaryDecoder.ReadString();
-        //            var long2 = binaryDecoder.ReadLong();
-        //
-        //            return result;
-        //        }
     }
 }
--- tests/AvroConvertTests/ComponentTests.cs
+++ tests/AvroConvertTests/ComponentTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace AvroConvertTests
 {
+    using System.Collections.Generic;
     using Xunit;
 
     public class ComponentTests
@@ -8,6 +9,31 @@
         public void Serialize_ThenDeserialize_ObjectsAreEqual()
         {
             //Arrange
+            User user = new User();
+            user.name = "Krzys";
+            user.favorite_color = "yellow";
+            user.favorite_number = null;
+
+            //Act
+            var serialized = AvroConvert.AvroConvert.Serialize(user);
+
+            var deserialized = AvroConvert.AvroConvert.Deserialize<User>(serialized);
+
+            //Assert
+            Assert.NotNull(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equal(user.name, deserialized.name);
+            Assert.Equal(user.favorite_color, deserialized.favorite_color);
+            Assert.Equal(user.favorite_number, deserialized.favorite_number);
+        }
+
+        [Fact]
+        public void SerializeList_ThenDeserialize_ListsAreEqual()
+        {
+            //Arrange
+
+            List<NestedTestClass> testList = new List<NestedTestClass>();
+
             User user = new User();
             user.name = "Krzys";
             user.favorite_color = "yellow";

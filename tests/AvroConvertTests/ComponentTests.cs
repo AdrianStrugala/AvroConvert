e8cd8d5eb2d34f0a0993e7591bf032c03252faac
Adrian Strugala <adrian.strugala@comparex.com>
2019-04-09 14:53:44 +0200

    added meaningfull component test - deserialization to imporove

--- tests/AvroConvertTests/ComponentTests.cs
+++ tests/AvroConvertTests/ComponentTests.cs
@@ -31,25 +31,36 @@
         public void SerializeList_ThenDeserialize_ListsAreEqual()
         {
             //Arrange
+            SomeTestClass someTestClass = new SomeTestClass
+            {
+                objectProperty = new NestedTestClass
+                {
+                    justSomeProperty = "spoko",
+                    andLongProperty = 2137
+                },
+                simpleProperty = 111111
+            };
 
-            List<NestedTestClass> testList = new List<NestedTestClass>();
+            SomeTestClass dupa2 = new SomeTestClass
+            {
+                objectProperty = new NestedTestClass
+                {
+                    justSomeProperty = "loko",
+                    andLongProperty = 2137
+                },
+                simpleProperty = 2135
+            };
 
-            User user = new User();
-            user.name = "Krzys";
-            user.favorite_color = "yellow";
-            user.favorite_number = null;
+            SomeTestClass[] someTestClasses = new SomeTestClass[2];
+            someTestClasses[0] = someTestClass;
+            someTestClasses[1] = dupa2;
 
             //Act
-            var serialized = AvroConvert.AvroConvert.Serialize(user);
-
-            var deserialized = AvroConvert.AvroConvert.Deserialize<User>(serialized);
+            var serialized = AvroConvert.AvroConvert.Serialize(someTestClasses);
+            var deserialized = AvroConvert.AvroConvert.Deserialize<List<SomeTestClass>>(serialized);
 
             //Assert
-            Assert.NotNull(serialized);
             Assert.NotNull(deserialized);
-            Assert.Equal(user.name, deserialized.name);
-            Assert.Equal(user.favorite_color, deserialized.favorite_color);
-            Assert.Equal(user.favorite_number, deserialized.favorite_number);
         }
     }
 }

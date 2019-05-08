1ef17a7e2590fc722d2db14ba3d9da4d7165d7fd
Adrian Strugala <adrian.strugala@comparex.com>
2019-05-08 15:02:30 +0200

    merging encoding logic into one place on a good truck

--- src/AvroConvert/Encoder/AbstractEncoder.cs
+++ src/AvroConvert/Encoder/AbstractEncoder.cs
@@ -8,7 +8,7 @@
     using System.Linq;
     using System.Reflection;
 
-    public abstract partial class AbstractEncoder
+    public abstract class AbstractEncoder
     {
         public Schema Schema { get; private set; }
 
@@ -288,23 +288,91 @@
             }
         }
 
+        public void EnsureRecordObject(RecordSchema recordSchema, object value)
+        {
+            if (value == null || !(value is GenericRecord) || !((value as GenericRecord).Schema.Equals(recordSchema)))
+            {
+                throw new AvroException("[GenericRecord] required to write against [Record] schema but found " + value.GetType());
+            }
+        }
 
-        protected abstract void EnsureRecordObject(RecordSchema recordSchema, object value);
+        public void WriteField(object record, string fieldName, int fieldPos, WriteItem writer,
+            IWriter encoder)
+        {
+            writer(((GenericRecord)record)[fieldName], encoder);
+        }
 
-        /// <summary>
-        /// Extracts the field value from the given object.
-        /// </summary>
-        /// <param name="value">The record value from which the field needs to be extracted</param>
-        /// <param name="fieldName">The name of the field in the record</param>
-        /// <param name="fieldPos">The position of field in the record</param>
-        /// <returns></returns>
-        protected abstract void WriteField(object record, string fieldName, int fieldPos, WriteItem writer, IWriter encoder);
+        public WriteItem ResolveEnum(EnumSchema es)
+        {
+            return (value, e) =>
+            {
+                if (value == null || !(value is GenericEnum) || !((value as GenericEnum).Schema.Equals(es)))
+                    throw new AvroException("[GenericEnum] required to write against [Enum] schema but found " + value.GetType());
+                e.WriteEnum(es.Ordinal((value as GenericEnum).Value));
+            };
+        }
 
-        /// <summary>
-        /// Serializes an enumeration.
-        /// </summary>
-        /// <param name="es">The EnumSchema for serialization</param>
-        protected abstract WriteItem ResolveEnum(EnumSchema es);
+        public void WriteFixed(FixedSchema es, object value, IWriter encoder)
+        {
+            if (value == null || !(value is GenericFixed) || !(value as GenericFixed).Schema.Equals(es))
+            {
+                throw new AvroException("[GenericFixed] required to write against [Fixed] schema but found " + value.GetType());
+            }
+
+            GenericFixed ba = (GenericFixed)value;
+            encoder.WriteFixed(ba.Value);
+        }
+
+        /*
+         * FIXME: This method of determining the Union branch has problems. If the data is IDictionary<string, object>
+         * if there are two branches one with record schema and the other with map, it choose the first one. Similarly if
+         * the data is byte[] and there are fixed and bytes schemas as branches, it choose the first one that matches.
+         * Also it does not recognize the arrays of primitive types.
+         */
+        public bool UnionBranchMatches(Schema sc, object obj)
+        {
+            if (obj == null && sc.Tag != Schema.Type.Null) return false;
+            switch (sc.Tag)
+            {
+                case Schema.Type.Null:
+                    return obj == null;
+                case Schema.Type.Boolean:
+                    return obj is bool;
+                case Schema.Type.Int:
+                    return obj is int;
+                case Schema.Type.Long:
+                    return obj is long;
+                case Schema.Type.Float:
+                    return obj is float;
+                case Schema.Type.Double:
+                    return obj is double;
+                case Schema.Type.Bytes:
+                    return obj is byte[];
+                case Schema.Type.String:
+                    return obj is string;
+                case Schema.Type.Error:
+                case Schema.Type.Record:
+                    //return obj is GenericRecord && (obj as GenericRecord).Schema.Equals(s);
+                    return obj is GenericRecord &&
+                           (obj as GenericRecord).Schema.SchemaName.Equals((sc as RecordSchema).SchemaName);
+                case Schema.Type.Enumeration:
+                    //return obj is GenericEnum && (obj as GenericEnum).Schema.Equals(s);
+                    return obj is GenericEnum &&
+                           (obj as GenericEnum).Schema.SchemaName.Equals((sc as EnumSchema).SchemaName);
+                case Schema.Type.Array:
+                    return obj is Array && !(obj is byte[]);
+                case Schema.Type.Map:
+                    return obj is IDictionary<string, object>;
+                case Schema.Type.Union:
+                    return false; // Union directly within another union not allowed!
+                case Schema.Type.Fixed:
+                    //return obj is GenericFixed && (obj as GenericFixed).Schema.Equals(s);
+                    return obj is GenericFixed &&
+                           (obj as GenericFixed).Schema.SchemaName.Equals((sc as FixedSchema).SchemaName);
+                default:
+                    throw new AvroException("Unknown schema type: " + sc.Tag);
+            }
+        }
 
         /// <summary>
         /// Serialized an array. The default implementation calls EnsureArrayObject() to ascertain that the
@@ -398,20 +466,9 @@
             throw new AvroException("Cannot find a match for " + obj.GetType() + " in " + us);
         }
 
-        /// <summary>
-        /// Serialized a fixed object. The default implementation requires that the value is
-        /// a GenericFixed object with an identical schema as es.
-        /// </summary>
-        /// <param name="es">The schema for serialization</param>
-        /// <param name="value">The value to be serialized</param>
-        /// <param name="encoder">The encoder for serialization</param>
-        protected abstract void WriteFixed(FixedSchema es, object value, IWriter encoder);
-
         private void error(Schema schema, Object value)
         {
             throw new AvroTypeException("Not a " + schema + ": " + value);
         }
-
-        protected abstract bool UnionBranchMatches(Schema sc, object obj);
     }
 }
--- src/AvroConvert/Encoder/GenericDatumWriter.cs
+++ src/AvroConvert/Encoder/GenericDatumWriter.cs
@@ -1,9 +1,6 @@
 ﻿namespace EhwarSoft.Avro.Encoder
 {
-    using Generic;
     using Schema;
-    using System;
-    using System.Collections.Generic;
 
     public class GenericDatumWriter : AbstractEncoder
     {
@@ -12,95 +9,6 @@
         public GenericDatumWriter(Schema schema) : base(schema, new ArrayAccess(), new DictionaryMapAccess())
         {
             _schema = schema;
-        }
-
-
-
-        protected override void EnsureRecordObject(RecordSchema recordSchema, object value)
-        {
-            if (value == null || !(value is GenericRecord) || !((value as GenericRecord).Schema.Equals(recordSchema)))
-            {
-                throw new AvroException("[GenericRecord] required to write against [Record] schema but found " + value.GetType());
-            }
-        }
-
-        protected override void WriteField(object record, string fieldName, int fieldPos, WriteItem writer,
-            IWriter encoder)
-        {
-            writer(((GenericRecord)record)[fieldName], encoder);
-        }
-
-        protected override WriteItem ResolveEnum(EnumSchema es)
-        {
-            return (value, e) =>
-            {
-                if (value == null || !(value is GenericEnum) || !((value as GenericEnum).Schema.Equals(es)))
-                    throw new AvroException("[GenericEnum] required to write against [Enum] schema but found " + value.GetType());
-                e.WriteEnum(es.Ordinal((value as GenericEnum).Value));
-            };
-        }
-
-        protected override void WriteFixed(FixedSchema es, object value, IWriter encoder)
-        {
-            if (value == null || !(value is GenericFixed) || !(value as GenericFixed).Schema.Equals(es))
-            {
-                throw new AvroException("[GenericFixed] required to write against [Fixed] schema but found " + value.GetType());
-            }
-
-            GenericFixed ba = (GenericFixed)value;
-            encoder.WriteFixed(ba.Value);
-        }
-
-
-        /*
-         * FIXME: This method of determining the Union branch has problems. If the data is IDictionary<string, object>
-         * if there are two branches one with record schema and the other with map, it choose the first one. Similarly if
-         * the data is byte[] and there are fixed and bytes schemas as branches, it choose the first one that matches.
-         * Also it does not recognize the arrays of primitive types.
-         */
-        protected override bool UnionBranchMatches(Schema sc, object obj)
-        {
-            if (obj == null && sc.Tag != Schema.Type.Null) return false;
-            switch (sc.Tag)
-            {
-                case Schema.Type.Null:
-                    return obj == null;
-                case Schema.Type.Boolean:
-                    return obj is bool;
-                case Schema.Type.Int:
-                    return obj is int;
-                case Schema.Type.Long:
-                    return obj is long;
-                case Schema.Type.Float:
-                    return obj is float;
-                case Schema.Type.Double:
-                    return obj is double;
-                case Schema.Type.Bytes:
-                    return obj is byte[];
-                case Schema.Type.String:
-                    return obj is string;
-                case Schema.Type.Error:
-                case Schema.Type.Record:
-                    //return obj is GenericRecord && (obj as GenericRecord).Schema.Equals(s);
-                    return obj is GenericRecord &&
-                           (obj as GenericRecord).Schema.SchemaName.Equals((sc as RecordSchema).SchemaName);
-                case Schema.Type.Enumeration:
-                    //return obj is GenericEnum && (obj as GenericEnum).Schema.Equals(s);
-                    return obj is GenericEnum &&
-                           (obj as GenericEnum).Schema.SchemaName.Equals((sc as EnumSchema).SchemaName);
-                case Schema.Type.Array:
-                    return obj is Array && !(obj is byte[]);
-                case Schema.Type.Map:
-                    return obj is IDictionary<string, object>;
-                case Schema.Type.Union:
-                    return false; // Union directly within another union not allowed!
-                case Schema.Type.Fixed:
-                    //return obj is GenericFixed && (obj as GenericFixed).Schema.Equals(s);
-                    return obj is GenericFixed &&
-                           (obj as GenericFixed).Schema.SchemaName.Equals((sc as FixedSchema).SchemaName);
-                default:
-                    throw new AvroException("Unknown schema type: " + sc.Tag);
-            }
         }
     }
 }

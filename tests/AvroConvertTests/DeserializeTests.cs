1727950f89a45e44046d328457b50ddeedf125cc
Adrian Strugala <strugala.adrian@gmail.com>
2019-05-22 18:42:30 +0200

    fixed test for deserialization

--- tests/AvroConvertTests/DeserializeTests.cs
+++ tests/AvroConvertTests/DeserializeTests.cs
@@ -1,33 +1,43 @@
 ﻿namespace AvroConvertTests
 {
+    using System.Collections.Generic;
     using AvroConvert;
     using Xunit;
 
     public class DeserializeTests
     {
         private readonly byte[] _avroBytes;
-        private readonly string _example2schema;
 
         public DeserializeTests()
         {
             _avroBytes = System.IO.File.ReadAllBytes("example2.avro");
-
-            _example2schema =
-                "{\"type\": \"record\", \"name\": \"User\", \"namespace\": \"example.avro\", \"fields\": [{\"type\": \"string\", \"name\": \"name\"}, {\"type\": [\"int\", \"null\"], \"name\": \"favorite_number\"}, {\"type\": [\"string\", \"null\"], \"name\": \"favorite_color\"}]}";
         }
 
         [Fact]
         public void Deserialize_ValidBytes_SetOfPropertiesAreReturned()
         {
             //Arrange
+            Dictionary<string, object> result1 = new Dictionary<string, object>();
+            result1.Add("name", "Alyssa");
+            result1.Add("favorite_number", 256);
+            result1.Add("favorite_color", null);
+
+            Dictionary<string, object> result2 = new Dictionary<string, object>();
+            result2.Add("name", "Ben");
+            result2.Add("favorite_number", 7);
+            result2.Add("favorite_color", "red");
+
+            List<object> expectedResult = new List<object>();
+            expectedResult.Add(result1);
+            expectedResult.Add(result2);
+
 
             //Act
             var result = AvroConvert.Deserialize(_avroBytes);
 
-            var xd = result;
 
             //Assert
-            //   Assert.Equal(_example2schema, schema);
+            Assert.Equal(expectedResult, result);
         }
 
     }

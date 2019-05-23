63553de8062a956baa0643d255503a4fa20f30fd
Adrian Strugala <adrian.strugala@comparex.com>
2019-05-23 12:49:23 +0200

    extracted union to separate resolver

--- src/AvroConvert/Write/Resolvers/Factory.cs
+++ src/AvroConvert/Write/Resolvers/Factory.cs
@@ -1,7 +1,5 @@
 ﻿namespace AvroConvert.Write.Resolvers
 {
-    using System.Collections.Generic;
-    using System.Linq;
     using Exceptions;
     using Generic;
     using Schema;
@@ -15,6 +13,7 @@
         private static readonly Record Record;
         private static readonly Enum Enum;
         private static readonly Fixed Fixed;
+        private static readonly Union Union;
 
         static Factory()
         {
@@ -25,7 +24,9 @@
             Record = new Record();
             Enum = new Enum();
             Fixed = new Fixed();
+            Union = new Union();
         }
+
         public static Encoder.WriteItem ResolveWriter(Schema schema)
         {
             switch (schema.Tag)
@@ -58,9 +59,11 @@
                 case Schema.Type.Map:
                     return Map.Resolve((MapSchema)schema);
                 case Schema.Type.Union:
-                    return ResolveUnion((UnionSchema)schema);
+                    return Union.Resolve((UnionSchema)schema);
                 default:
-                    return (v, e) => throw new AvroTypeMismatchException($"Tried to write against [{schema}] schema, but found [{v.GetType()}] type");
+                    return (v, e) =>
+                        throw new AvroTypeMismatchException(
+                            $"Tried to write against [{schema}] schema, but found [{v.GetType()}] type");
             }
         }
 
@@ -78,107 +81,11 @@
                 value = default(S);
             }
 
-            if (!(value is S)) throw new AvroTypeMismatchException($"[{ typeof(S)}] required to write against [{tag.ToString()}] schema but found " + value.GetType());
+            if (!(value is S))
+                throw new AvroTypeMismatchException(
+                    $"[{typeof(S)}] required to write against [{tag.ToString()}] schema but found " + value.GetType());
 
             writer((S)value);
-        }
-
-
-        /*
-         * FIXME: This method of determining the Union branch has problems. If the data is IDictionary<string, object>
-         * if there are two branches one with record schema and the other with map, it choose the first one. Similarly if
-         * the data is byte[] and there are fixed and bytes schemas as branches, it choose the first one that matches.
-         * Also it does not recognize the arrays of primitive types.
-         */
-        public static bool UnionBranchMatches(Schema sc, object obj)
-        {
-            if (obj == null && sc.Tag != Schema.Type.Null) return false;
-            switch (sc.Tag)
-            {
-                case Schema.Type.Null:
-                    return obj == null;
-                case Schema.Type.Boolean:
-                    return obj is bool;
-                case Schema.Type.Int:
-                    return obj is int;
-                case Schema.Type.Long:
-                    return obj is long;
-                case Schema.Type.Float:
-                    return obj is float;
-                case Schema.Type.Double:
-                    return obj is double;
-                case Schema.Type.Bytes:
-                    return obj is byte[];
-                case Schema.Type.String:
-                    return obj is string;
-                case Schema.Type.Error:
-                case Schema.Type.Record:
-                    //return obj is GenericRecord && (obj as GenericRecord)._schema.Equals(s);
-                    return obj is GenericRecord &&
-                           (obj as GenericRecord).Schema.SchemaName.Equals((sc as RecordSchema).SchemaName);
-                case Schema.Type.Enumeration:
-                    //return obj is GenericEnum && (obj as GenericEnum)._schema.Equals(s);
-                    return obj is GenericEnum &&
-                           (obj as GenericEnum).Schema.SchemaName.Equals((sc as EnumSchema).SchemaName);
-                case Schema.Type.Array:
-                    return obj is System.Array && !(obj is byte[]);
-                case Schema.Type.Map:
-                    return obj is IDictionary<string, object>;
-                case Schema.Type.Union:
-                    return false; // Union directly within another union not allowed!
-                case Schema.Type.Fixed:
-                    //return obj is GenericFixed && (obj as GenericFixed)._schema.Equals(s);
-                    return obj is GenericFixed &&
-                           (obj as GenericFixed).Schema.SchemaName.Equals((sc as FixedSchema).SchemaName);
-                default:
-                    throw new AvroException("Unknown schema type: " + sc.Tag);
-            }
-        }
-
-
-        private static Encoder.WriteItem ResolveUnion(UnionSchema unionSchema)
-        {
-            var branchSchemas = unionSchema.Schemas.ToArray();
-            var branchWriters = new Encoder.WriteItem[branchSchemas.Length];
-            int branchIndex = 0;
-            foreach (var branch in branchSchemas)
-            {
-                branchWriters[branchIndex++] = ResolveWriter(branch);
-            }
-
-
-            return (v, e) => WriteUnion(unionSchema, branchSchemas, branchWriters, v, e);
-        }
-
-        /// <summary>
-        /// Resolves the given value against the given UnionSchema and serializes the object against
-        /// the resolved schema member.
-        /// </summary>
-        /// <param name="us">The UnionSchema to resolve against</param>
-        /// <param name="value">The value to be serialized</param>
-        /// <param name="encoder">The encoder for serialization</param>
-        private static void WriteUnion(UnionSchema unionSchema, Schema[] branchSchemas, Encoder.WriteItem[] branchWriters, object value, IWriter encoder)
-        {
-            int index = ResolveUnion(unionSchema, branchSchemas, value);
-            encoder.WriteUnionIndex(index);
-            branchWriters[index](value, encoder);
-        }
-
-        /// <summary>
-        /// Finds the branch within the given UnionSchema that matches the given object. The default implementation
-        /// calls Matches() method in the order of branches within the UnionSchema. If nothing matches, throws
-        /// an exception.
-        /// </summary>
-        /// <param name="us">The UnionSchema to resolve against</param>
-        /// <param name="obj">The object that should be used in matching</param>
-        /// <returns></returns>
-        private static int ResolveUnion(UnionSchema us, Schema[] branchSchemas, object obj)
-        {
-            for (int i = 0; i < branchSchemas.Length; i++)
-            {
-                if (UnionBranchMatches(branchSchemas[i], obj)) return i;
-            }
-            throw new AvroException("Cannot find a match for " + obj.GetType() + " in " + us);
         }
     }
 }
---
+++ src/AvroConvert/Write/Resolvers/Union.cs
@@ -0,0 +1,91 @@
+﻿namespace AvroConvert.Write.Resolvers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Generic;
+    using Schema;
+
+    public class Union
+    {
+        public Encoder.WriteItem Resolve(UnionSchema unionSchema)
+        {
+            var branchSchemas = unionSchema.Schemas.ToArray();
+            var branchWriters = new Encoder.WriteItem[branchSchemas.Length];
+            int branchIndex = 0;
+            foreach (var branch in branchSchemas)
+            {
+                branchWriters[branchIndex++] = Factory.ResolveWriter(branch);
+            }
+
+
+            return (v, e) => WriteUnion(unionSchema, branchSchemas, branchWriters, v, e);
+        }
+
+        /*TODO:
+         * FIXME: This method of determining the Union branch has problems. If the data is IDictionary<string, object>
+         * if there are two branches one with record schema and the other with map, it choose the first one. Similarly if
+         * the data is byte[] and there are fixed and bytes schemas as branches, it choose the first one that matches.
+         * Also it does not recognize the arrays of primitive types.
+         */
+        private bool UnionBranchMatches(Schema sc, object obj)
+        {
+            if (obj == null && sc.Tag != Schema.Type.Null) return false;
+            switch (sc.Tag)
+            {
+                case Schema.Type.Null:
+                    return obj == null;
+                case Schema.Type.Boolean:
+                    return obj is bool;
+                case Schema.Type.Int:
+                    return obj is int;
+                case Schema.Type.Long:
+                    return obj is long;
+                case Schema.Type.Float:
+                    return obj is float;
+                case Schema.Type.Double:
+                    return obj is double;
+                case Schema.Type.Bytes:
+                    return obj is byte[];
+                case Schema.Type.String:
+                    return obj is string;
+                case Schema.Type.Error:
+                case Schema.Type.Record:
+                    //return obj is GenericRecord && (obj as GenericRecord)._schema.Equals(s);
+                    return obj is GenericRecord &&
+                           (obj as GenericRecord).Schema.SchemaName.Equals((sc as RecordSchema).SchemaName);
+                case Schema.Type.Enumeration:
+                    //return obj is GenericEnum && (obj as GenericEnum)._schema.Equals(s);
+                    return obj is GenericEnum &&
+                           (obj as GenericEnum).Schema.SchemaName.Equals((sc as EnumSchema).SchemaName);
+                case Schema.Type.Array:
+                    return obj is System.Array && !(obj is byte[]);
+                case Schema.Type.Map:
+                    return obj is IDictionary<string, object>;
+                case Schema.Type.Union:
+                    return false; // Union directly within another union not allowed!
+                case Schema.Type.Fixed:
+                    //return obj is GenericFixed && (obj as GenericFixed)._schema.Equals(s);
+                    return obj is GenericFixed &&
+                           (obj as GenericFixed).Schema.SchemaName.Equals((sc as FixedSchema).SchemaName);
+                default:
+                    throw new AvroException("Unknown schema type: " + sc.Tag);
+            }
+        }
+
+        private void WriteUnion(UnionSchema unionSchema, Schema[] branchSchemas, Encoder.WriteItem[] branchWriters, object value, IWriter encoder)
+        {
+            int index = ResolveUnion(unionSchema, branchSchemas, value);
+            encoder.WriteUnionIndex(index);
+            branchWriters[index](value, encoder);
+        }
+
+        private int ResolveUnion(UnionSchema us, Schema[] branchSchemas, object obj)
+        {
+            for (int i = 0; i < branchSchemas.Length; i++)
+            {
+                if (UnionBranchMatches(branchSchemas[i], obj)) return i;
+            }
+            throw new AvroException("Cannot find a match for " + obj.GetType() + " in " + us);
+        }
+    }
+}

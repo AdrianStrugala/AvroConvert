f64a85d0a9f6f3e205b612283f2d318cd5fdd80c
Adrian <strugala.adrian@gmail.com>
2020-02-06 10:42:30 +0100

    added more tests for enum

--- tests/AvroConvertTests/Component/EnumTests.cs
+++ tests/AvroConvertTests/Component/EnumTests.cs
@@ -1,4 +1,6 @@
-﻿using AutoFixture;
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
 using SolTechnology.Avro;
 using Xunit;
 
@@ -30,6 +32,44 @@ namespace AvroConvertTests.Component
             Assert.NotNull(result);
             Assert.NotNull(deserialized);
             Assert.Equal(toSerialize.enumVariable, deserialized.enumVariable);
+            Assert.Equal(toSerialize, deserialized);
+        }
+
+        [Fact]
+        public void Component_SerializeEnum_ResultIsTheSameAsInput()
+        {
+            //Arrange
+            TestEnum toSerialize = _fixture.Create<TestEnum>();
+
+
+            //Act
+            var result = AvroConvert.Serialize(toSerialize);
+
+            TestEnum deserialized = AvroConvert.Deserialize<TestEnum>(result);
+
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotNull(deserialized);
+            Assert.Equal(toSerialize, deserialized);
+        }
+
+        [Fact]
+        public void Component_SerializeListOfEnums_ResultIsTheSameAsInput()
+        {
+            //Arrange
+            List<TestEnum> toSerialize = _fixture.CreateMany<TestEnum>(20).ToList();
+
+
+            //Act
+            var result = AvroConvert.Serialize(toSerialize);
+
+            var deserialized = AvroConvert.Deserialize<List<TestEnum>>(result);
+
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotNull(deserialized);
             Assert.Equal(toSerialize, deserialized);
         }
     }

75ebd22829c67538606b068d49c6f89c7511d74c
Adrian <strugala.adrian@gmail.com>
2020-03-20 14:02:49 +0100

    remove references to AutoMapper

--- src/AvroConvert/AvroConvert.Deserialize.cs
+++ src/AvroConvert/AvroConvert.Deserialize.cs
@@ -17,23 +17,12 @@
 
 using System;
 using System.IO;
-using AutoMapper;
-using SolTechnology.Avro.Models;
 using SolTechnology.Avro.Read;
-using SolTechnology.Avro.Read.AutoMapperConverters;
 
 namespace SolTechnology.Avro
 {
     public static partial class AvroConvert
     {
-        static AvroConvert()
-        {
-            Mapper.Initialize(cfg =>
-                              {
-                                  cfg.CreateMap<long, DateTime>().ConvertUsing(new DateTimeConverter());
-                                  cfg.CreateMap<Fixed, Guid>().ConvertUsing(new GuidConverter());
-                              });
-        }
 
         public static T Deserialize<T>(byte[] avroBytes)
         {

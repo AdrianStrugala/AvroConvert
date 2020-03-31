180c9bcd9bb450001f6ed4af6c6adcc833123fe7
Adrian Strugala <strugala.adrian@gmail.com>
2020-03-31 17:20:06 +0200

    added HttpClientAvroExtensions class to Http project

---
+++ src/SolTechnology.Avro.Http/HttpClientAvroExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace SolTechnology.Avro.Http
+{
+    public static class HttpClientAvroExtensions
+    {
+        public static async Task<HttpResponseMessage> PostAsAvro(this HttpClient httpClient, string requestUri, object content)
+        {
+            var body = new ByteArrayContent(AvroConvert.Serialize(content));
+            body.Headers.ContentType = new MediaTypeHeaderValue("application/avro");
+            return await httpClient.PostAsync(requestUri, body);
+        }
+
+        public static async Task<T> GetAsAvro<T>(this HttpClient httpClient, string requestUri)
+        {
+            var response = await httpClient.GetByteArrayAsync(requestUri);
+            T result = AvroConvert.Deserialize<T>(response);
+            return result;
+        }
+    }
+}

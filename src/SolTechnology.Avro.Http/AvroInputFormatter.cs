7418abc6f40e9763e2169f71798dfb1ce091b326
Adrian <strugala.adrian@gmail.com>
2020-03-31 12:47:38 +0200

    moved SolTechnology.Avro.Http to AvroConvert repo

---
+++ src/SolTechnology.Avro.Http/AvroInputFormatter.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+
+namespace SolTechnology.Avro.Http
+{
+    public class AvroInputFormatter : InputFormatter
+    {
+        public AvroInputFormatter()
+        {
+            this.SupportedMediaTypes.Clear();
+
+            this.SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/avro"));
+        }
+
+        public override Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                context.HttpContext.Request.Body.CopyTo(ms);
+                var type = context.ModelType;
+
+                object result = AvroConvert.Deserialize(ms.ToArray(), type);
+                return InputFormatterResult.SuccessAsync(result);
+            }
+        }
+    }
+}
---
+++ src/SolTechnology.Avro.Http/AvroOutputFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+
+namespace SolTechnology.Avro.Http
+{
+    public class AvroOutputFormatter : OutputFormatter
+    {
+        public AvroOutputFormatter()
+        {
+            this.SupportedMediaTypes.Clear();
+
+            this.SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/avro"));
+        }
+
+        public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
+        {
+            var avroBody = AvroConvert.Serialize(context.Object);
+
+            var response = context.HttpContext.Response;
+            response.ContentLength = avroBody.Length;
+
+            await response.Body.WriteAsync(avroBody);
+        }
+    }
+}

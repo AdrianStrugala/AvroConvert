fa0a1e8936111c962c60160d54345d356b1eb511
Adrian <strugala.adrian@gmail.com>
2020-04-22 12:46:22 +0200

    Added http client

---
+++ tests/Http.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using SolTechnology.Avro.Http;
+
+namespace Http.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Http Client running!");
+
+            HttpClient httpClient = new HttpClient();
+            var avroResult = httpClient.GetAsAvro<List<WeatherForecast>>("https://localhost:2137/WeatherForecast").Result;
+
+            Console.WriteLine(avroResult.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}

178631cdc645dee5d0d66018c6d80fe47b96c54e
Adrian <strugala.adrian@gmail.com>
2020-05-08 17:38:44 +0200

    added test for proving hipothesis about jsons

---
+++ tests/AvroConvertTests/AvroToJson/JsonTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace AvroConvertTests.AvroToJson
+{
+    public class JsonTests
+    {
+        public void Json_SerializeObjectAndDictionaryWithItemsNamedLikeProperties_TheyAreEqual()
+        {
+            //Arrange
+            var user = new User();
+            user.favorite_color = "blue";
+            user.favorite_number = 2137;
+            user.name = "red";
+
+            var userDictionary = new Dictionary<string, object>();
+            userDictionary.Add("name", "red");
+            userDictionary.Add("favorite_number", 2137);
+            userDictionary.Add("favorite_color", "blue");
+
+
+            //Act
+            var json1 = JsonConvert.SerializeObject(user);
+
+            var json2 = JsonConvert.SerializeObject(userDictionary);
+
+
+            //Assert
+            Assert.Equal(json1, json2);
+        }
+    }
+}

e62223ad72db5b60d0a634c8d5df878679d39d95
Adrian <strugala.adrian@gmail.com>
2020-06-16 12:19:56 +0200

    draft

---
+++ src/AvroConvert/AvroConvert.DeserializeByLine.cs
@@ -0,0 +1,48 @@
+﻿#region license
+/**Copyright (c) 2020 Adrian Strugała
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#endregion
+
+using System;
+using System.IO;
+using SolTechnology.Avro.Read;
+
+namespace SolTechnology.Avro
+{
+    public static partial class AvroConvert
+    {
+        public static T OpenDeserializer<T>(Stream stream)
+        {
+            var reader = Decoder.OpenReader(stream, Schema.Schema.Parse(GenerateSchema(typeof(T))));
+
+            return reader.Read<T>();
+        }
+    }
+
+    public class LineReader<T>
+    {
+        private readonly Decoder decoder;
+
+        internal LineReader(Decoder decoder)
+        {
+            this.decoder = decoder;
+        }
+
+        public T ReadNext()
+        {
+            decoder.Read<>()
+        }
+    }
+}

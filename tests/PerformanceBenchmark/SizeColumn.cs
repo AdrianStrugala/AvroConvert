f47aaf7505bdfcaf35feefe01d17aa920b5be4e5
Adrian Strugala <strugala.adrian@gmail.com>
2021-08-07 17:37:22 +0200

    I love dependencies

---
+++ tests/PerformanceBenchmark/SizeColumn.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace GrandeBenchmark
+{
+    public class FileSizeColumn : IColumn
+    {
+        public string Id => nameof(FileSizeColumn);
+
+        public string ColumnName => "FileSize";
+
+        public string Legend => "Allocated memory on disk after all records are serialized (1KB = 1024B)";
+
+        public UnitType UnitType => UnitType.Size;
+
+        public bool AlwaysShow => true;
+
+        public ColumnCategory Category => ColumnCategory.Metric;
+
+        public int PriorityInCategory => 0;
+
+        public bool IsNumeric => true;
+
+        public bool IsAvailable(Summary summary) => true;
+
+        public bool IsDefault(Summary summary, BenchmarkCase benchmarkCase) => false;
+
+        public string GetValue(Summary summary, BenchmarkCase benchmarkCase) => GetValue(summary, benchmarkCase, SummaryStyle.Default);
+
+        public string GetValue(Summary summary, BenchmarkCase benchmarkCase, SummaryStyle style)
+        {
+            var benchmarkName = benchmarkCase.Descriptor.WorkloadMethod.Name.ToLower();
+
+            var filename = $"disk-size.{benchmarkName}.txt";
+            var path = $"C:\\test\\{filename}";
+            return File.Exists(path) ? File.ReadAllText(path) : $"file not found";
+        }
+
+        public override string ToString() => ColumnName;
+    }
+}

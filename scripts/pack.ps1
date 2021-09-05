0ccd5fac5ae237cc2c3717f353d676203e7692f0
Adrian Strugala <strugala.adrian@gmail.com>
2021-09-05 12:13:15 +0200

    pack script nearly done

---
+++ scripts/pack.ps1
@@ -0,0 +1,12 @@
+﻿$csProjName = 'AvroConvert.csproj';
+
+cd E:\workspace\AvroConvert\scripts
+
+cd ../src/AvroConvert
+
+((Get-Content -Path $csProjName -Raw) -replace '3.1.5', '21.37') | Set-Content -Path $csProjName
+dotnet build -c Release
+dotnet pack -c Release
+
+cd ../../
+Get-ChildItem .\src\AvroConvert\obj\Release -Filter *.nuspec | Copy-Item -Destination . -Force -PassThru

31ff6d04eff12d6cc48c17cada0d5b8c8d5315c8
Adrian Strugala <strugala.adrian@gmail.com>
2023-02-27 12:51:03 +0100

    reproduced issue

---
+++ tests/AvroConvertTests/Headless/Proto/HeadlessProtoFileTest.cs
@@ -0,0 +1,36 @@
+﻿using SolTechnology.Avro;
+using Xunit;
+
+namespace AvroConvertComponentTests.Headless.Proto
+{
+    // from issue: https://github.com/AdrianStrugala/AvroConvert/issues/95
+    public class HeadlessProtoFileTest
+    {
+        [Fact]
+        public void Component_SerializeHeadlessBiggerObjectAndReadSmaller_NoError()
+        {
+            //Arrange
+            string schemaString =
+                "{\r\n  \"name\": \"SampleProtoGeneratedClass\",\r\n  \"namespace\": \"Test\",\r\n  \"type\": \"record\",\r\n  \"fields\": [\r\n    {\r\n      \"name\": \"Type\",\r\n      \"type\": \"string\"\r\n    },\r\n    {\r\n      \"name\": \"Version\",\r\n      \"type\": \"int\"\r\n    },\r\n    {\r\n      \"name\": \"CorrelationId\",\r\n      \"type\": \"string\"\r\n    },\r\n    {\r\n      \"name\": \"OperationId\",\r\n      \"type\": \"string\"\r\n    },\r\n    {\r\n      \"name\": \"MessageId\",\r\n      \"type\": \"string\"\r\n    },\r\n    {\r\n      \"name\": \"Timestamp\",\r\n      \"type\": \"string\"\r\n    }\r\n  ]\r\n}";
+
+            var value = new SampleProtoGeneratedClass
+            {
+                CorrelationId = "cor",
+                MessageId = "mes",
+                OperationId = "ope",
+                Timestamp = "time",
+                Type = "type",
+                Version = 2
+            };
+
+            //Act
+            var serialized = AvroConvert.SerializeHeadless(value, schemaString);
+            var deserialized = AvroConvert.DeserializeHeadless<SampleProtoGeneratedClass>(serialized, schemaString);
+
+            //Assert
+            Assert.NotNull(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equal(value, deserialized);
+        }
+    }
+}

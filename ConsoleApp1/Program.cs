9fed6050001cbd064412c9429df8122c5bce1029
Adrian Strugala <strugala.adrian@gmail.com>
2023-04-26 11:57:01 +0200

    very simple framework app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolTechnology.Avro;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var xd = AvroConvert.GenerateSchema(typeof(string));
+
+            var x = xd;
+        }
+    }
+}
